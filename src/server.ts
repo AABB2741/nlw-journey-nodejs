6834040fe8fb91f80e20afa2fbe0c35b0b63c88d
Mario <mariosantos88477364484@gmail.com>
2024-07-12 21:47:28 -0300

    Aula 01

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,23 @@
 import { fastify } from "fastify";
+import { createTrip } from "./routes/create-trip";
+import {
+    serializerCompiler,
+    validatorCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+    origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({
     port: 3333,
